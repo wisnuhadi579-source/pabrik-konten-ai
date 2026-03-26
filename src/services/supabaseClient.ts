6395f93882b6597e3a99778b17ce116d077085a3
Pakar Digital <wisnuhadi579@gmail.com>
2026-03-26 23:51:19 +0700

    Update supabaseClient.ts

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ajtefnkjdzavwacgqkri.supabase.co";
 
 const supabaseKey =
-  "YOUR_ANON_KEY"; // ⚠️ tetap pakai anon key di frontend
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqdGVmbmtqZHphdndhY2dxa3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2MzE2NjIsImV4cCI6MjA4NTIwNzY2Mn0.KjQDTGLPuaDsZM5dSipNYZcfr45CuRooFNSCRXDdGuY"; // ⚠️ tetap pakai anon key di frontend
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
